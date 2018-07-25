89d19220e23d1f2def893059b73f4299ebbbdb19
Steffen Loesch <stloesch@microsoft.com>
2018-07-24 17:55:59 -0700

    Adding sample project that uses private build of https://github.com/steffenloesch/moq

---
+++ MoqUsageTest1/Test1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace MoqUsageTest1
+{
+    class Test1
+    {
+        public static void RunTest()
+        {
+            Console.WriteLine("Test");
+            //The line below gives error "The name 'Mock' does not exist in the current context"
+            //var fooMock = Mock.Of<IFoo>(); 
+        }
+
+    }
+
+    interface IFoo
+    {
+        int FooInt();
+    }
+}
